77858e8331372483a150fe1b0c4476241e595778
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2019-05-04 00:31:28 -0300

    Include a tip of how to use knock to debug errors

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -24,3 +24,25 @@ Knock::AuthToken.class_eval do
     entity_class.find_by(key_to_find => @payload['sub'])
   end
 end
+
+
+# Uncomment this method if you're receiving odds 401 errors.
+# What I changed here is to raise an error through this method
+# if Knock::AuthToken raises an error. Here, silent errors make
+# hard to undertand the problem origin (like the use of a wrong algorithm
+# for token signature, for instance).
+# Knock::Authenticable.module_eval do
+#   def define_current_entity_getter(entity_class, getter_name)
+#     unless self.respond_to?(getter_name)
+#       memoization_var_name = "@_#{getter_name}"
+#       self.class.send(:define_method, getter_name) do
+#         unless instance_variable_defined?(memoization_var_name)
+#           current = Knock::AuthToken.new(token: token).entity_for(entity_class)
+
+#           instance_variable_set(memoization_var_name, current)
+#         end
+#         instance_variable_get(memoization_var_name)
+#       end
+#     end
+#   end
+# end
